42cb45dafbc793ae9625a5d00c5f74f184128816
CHtl_god <752394145@qq.com>
2018-01-31 17:03:07 +0800

    首页基本完成，文章详情进行中

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
         <my-header></my-header>
         <div class="index my-container">
             <div class="row">
+                <!--左侧布局-->
                 <div class="col-8 main">
                     <!--banner-->
                     <div v-swiper:mySwiper="swiperOption" class="banner">
@@ -99,10 +100,145 @@
                                     </div>
                                 </div>
                             </li>
+                            <li>
+                                <div class="content">
+                                    <div class="author">
+                                        <nuxt-link to="/p/123" class="avatar">
+                                            <img src="../assets/img/default-avatar.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/p" class="nickname">
+                                                萌薇
+                                            </nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
+                                    </div>
+                                    <nuxt-link to="/p" class="title">
+                                        如何把书中的知识转化为能力?这是我见过最好的回答
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，
+                                        于是市场上出现很多读书方法，比如速度、多读、慢读、列读……
+                                        不管你采用哪一种读书方法，
+                                        最终都要回归到“行动”二字上，正如陆...
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                            短片小说
+                                        </nuxt-link>
+                                        <nuxt-link to="/p" class="tag">
+                                            <i class="fa fa-eye">
+                                                579
+                                            </i>
+                                        </nuxt-link>
+                                        <nuxt-link to="/p" class="tag">
+                                            <i class="fa fa-comment"></i>
+                                            200
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            546
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content">
+                                    <div class="author">
+                                        <nuxt-link to="/p/123" class="avatar">
+                                            <img src="../assets/img/default-avatar.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/p" class="nickname">
+                                                萌薇
+                                            </nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
+                                    </div>
+                                    <nuxt-link to="/p" class="title">
+                                        如何把书中的知识转化为能力?这是我见过最好的回答
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，
+                                        于是市场上出现很多读书方法，比如速度、多读、慢读、列读……
+                                        不管你采用哪一种读书方法，
+                                        最终都要回归到“行动”二字上，正如陆...
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                            短片小说
+                                        </nuxt-link>
+                                        <nuxt-link to="/p" class="tag">
+                                            <i class="fa fa-eye">
+                                                579
+                                            </i>
+                                        </nuxt-link>
+                                        <nuxt-link to="/p" class="tag">
+                                            <i class="fa fa-comment"></i>
+                                            200
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            546
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="have-img">
+                                <nuxt-link to="/p/123" class="wrap-img">
+                                    <img src="../assets/img/note1.jpg" alt="">
+                                </nuxt-link>
+                                <div class="content">
+                                    <div class="author">
+                                        <nuxt-link to="/p/123" class="avatar">
+                                            <img src="../assets/img/default-avatar.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/p" class="nickname">
+                                                萌薇
+                                            </nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
+                                    </div>
+                                    <nuxt-link to="/p" class="title">
+                                        如何把书中的知识转化为能力?这是我见过最好的回答
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，
+                                        于是市场上出现很多读书方法，比如速度、多读、慢读、列读……
+                                        不管你采用哪一种读书方法，
+                                        最终都要回归到“行动”二字上，正如陆...
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                            短片小说
+                                        </nuxt-link>
+                                        <nuxt-link to="/p" class="tag">
+                                            <i class="fa fa-eye">
+                                                579
+                                            </i>
+                                        </nuxt-link>
+                                        <nuxt-link to="/p" class="tag">
+                                            <i class="fa fa-comment"></i>
+                                            200
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            546
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
                         </ul>
                     </div>
+                    <!--阅读更多-->
+                    <nuxt-link to="/" class="load-more">
+                        阅读更多
+                    </nuxt-link>
                 </div>
+                <!--右侧布局-->
                 <div class="col-4 aside">
+                    <!--分类条-->
                     <div class="board">
                         <nuxt-link to="/">
                             <img src="../assets/img/line1.png" alt="">
@@ -120,9 +256,75 @@
                             <img src="../assets/img/line5.png" alt="">
                         </nuxt-link>
                     </div>
+                    <!--推荐作者-->
+                    <div class="recommend-author">
+                        <!--二维码-->
+                        <nuxt-link to="/u" class="download">
+                            <img src="../assets/img/mywei.png" alt="">
+                            <div class="info">
+                                <div class="title">
+                                    咨询与打赏
+                                    <i class="fa fa-angle-right"></i>
+                                </div>
+                                <div class="description">
+                                    作者微信:lct171314
+                                </div>
+                            </div>
+                        </nuxt-link>
+                        <div class="title">
+                            <span>推荐作者</span>
+                            <a href="javascript:void(0)" class="page-change" @click="rotate">
+                                <i ref="refresh" class="fa fa-refresh"></i>
+                                <!--选取dom元素  加ref-->
+                                换一批
+                            </a>
+                        </div>
+                        <ul class="list">
+                            <li>
+                                <nuxt-link to="/" class="avatar">
+                                    <img src="../assets/img/default-avatar.jpg" alt="">
+                                </nuxt-link>
+                                <nuxt-link to="/" class="follow">
+                                    <i class="fa fa-plus"></i>
+                                    关注
+                                </nuxt-link>
+                                <nuxt-link to="/" class="name">
+                                    战小狼
+                                </nuxt-link>
+                                <p>
+                                    写了152.9k字·7.2k喜欢
+                                </p>
+                            </li>
+                        </ul>
+                        <nuxt-link to="/" class="find-more">
+                            查看全部
+                            <i class="fa fa-angle-right"></i>
+                        </nuxt-link>
+                    </div>
                 </div>
             </div>
         </div>
+        <footer class="my-container">
+            <div class="row">
+                <div class="col-xs-17 main">
+                    <nuxt-link to="/footer">
+                        作者：刘昶廷
+                    </nuxt-link>
+                    <em>&emsp;·&emsp;</em>
+                    <nuxt-link to="/footer">
+                        开始时间：2018-01-20
+                    </nuxt-link>
+                    <em>&emsp;·&emsp;</em>
+                    <nuxt-link to="/footer">
+                        院校：中北大学软件学院
+                    </nuxt-link>
+                    <div class="icp">
+                        @刘昶廷·于2018年6月30日毕业于中北大学软件学院
+                        联系电话：15735655136·QQ：752394145·微信：lct171314
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -154,13 +356,15 @@
                         prevEl: '.swiper-button-prev',
                     },
                 },
-
+                deg:360
+            }
+        },
+        methods:{
+            rotate:function() {
+                this.deg = this.deg +360
+                this.$refs.refresh.style.transform=`rotate(${this.deg}deg)`
             }
         }
     }
 </script>
-<style>
-    .swiper-button-next{
 
-    }
-</style>
---
+++ pages/p/_id.vue
@@ -0,0 +1,196 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="note">
+            <!--文章区域-->
+            <div class="post">
+                <!--正文-->
+                <div class="article">
+                    <h1 class="title">月薪三千，就该去死么</h1>
+                    <!--作者-->
+                    <div class="author">
+                        <nuxt-link to="" class="avatar">
+                            <img src="../../assets/img/default-avatar.jpg" alt="">
+                        </nuxt-link>
+                        <div class="info">
+                            <span class="name">测试用户</span>
+                            <a href="javascript:void(0)" class="btn btn-success " :class="follow"
+                              @click="haveSeen" @mouseenter="mouseenter" @mouseleave="mouseleave">
+                                <i ref="icon" class="fa" :class="icon"></i>
+                                <span ref="see">关注</span>
+                            </a>
+                            <div class="meta">
+                                <span>2018.01.23 16:09</span>
+                                <span>字数 2273</span>
+                                <span>阅读 882</span>
+                                <span>评论 7</span>
+                                <span>喜欢 34</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--内容-->
+                    <div class="show-content">
+                        <p>可能是春节快要到了，游子回家的旋律吹响，一年一度的“比头疼大赛”又要拉开大幕。比什么？比工作，比资产，比儿女，比婚姻。
+                            都市人携带着远方不为所知的辛苦，在同学相聚时，多了些虚伪；在朋友重逢时，少了些真诚。</p>
+                        <p>家乡是个让人又爱又恨的地方。有时，常看见有人自嘲：在外大老板，回乡二胖子；在外大哥大，
+                            回镇三孙子；在外大名媛，回村小豆子；在外喜当爹，回家好多爹——亲爹不倒，干爹飘飘。</p>
+                    </div>
+                    <!--小脚-->
+                    <div class="show-foot">
+                        <nuxt-link class="notebook" to="/nb/123">
+                            <i class="fa fa-book"></i>
+                            <span>得及-魏宏志</span>
+                        </nuxt-link>
+                        <div class="copyright" data-toggle="tooltip" data-html="true" data-original-title="转载请联系作者获得授权，并标注“简书作者”。">
+                            © 著作权归作者所有
+                        </div>
+                        <!--<div class="modal-wrap" data-report-note="">-->
+                            <!--<a id="report-modal">举报文章</a>-->
+                        <!--</div>-->
+                    </div>
+                    <!--下部作者-->
+                    <div class="follow-detail">
+                        <div class="info">
+                            <nuxt-link class="avatar" to="/u/123">
+                                <img src="../../assets/img/default-avatar.jpg">
+                            </nuxt-link>
+                            <a class="btn btn-success" href="javascript:void(0)"
+                               :class="follow"
+                               @click="haveSeen" @mouseenter="mouseenter" @mouseleave="mouseleave">
+                                <i ref="icon2" class="fa" :class="icon"></i>
+                                <span ref="see2">关注</span>
+                            </a>
+                            <nuxt-link class="title" to="/u/123">
+                                测试用户
+                            </nuxt-link>
+                            <i class="fa fa-mars"></i>
+                            <p>写了 46873 字，被 26 人关注，获得了 95 个喜欢</p>
+                        </div>
+                        <div class="signature">
+                            为城市里的不安和孤寂发声。
+                            微信公众号：得及
+                            为城市里的不安和孤寂发声。
+                            微信公众号：得及 为城市里的不安和孤寂发声。
+                            微信公众号：得及 为城市里的不安和孤寂发声。
+                            微信公众号：得及 为城市里的不安和孤寂发声。
+                            微信公众号：得及 为城市里的不安和孤寂发声。
+                            微信公众号：得及 为城市里的不安和孤寂发声。
+                            微信公众号：得及 为城市里的不安和孤寂发声。
+                            微信公众号：得及
+                        </div>
+                    </div>
+                    <!--喜欢和分享-->
+                    <div class="meta-bottom">
+                        <div class="like">
+                            <div class="btn" :class="active" @click="changeActive">
+                                <div class="btn-like">
+                                    <a href="javascript:void(0)" :class="{color:isColor}">
+                                        <i class="fa fa-heart-o"></i>
+                                        喜欢
+                                    </a>
+                                </div>
+                                <div class="modal-wrap" :class="{color:isColor}">
+                                    <a :class="{color:isColor}">
+                                        <span ref="spanNum">55</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="share-group">
+                            <a href="javascript:void(0)" class="share-circle">
+                                <i class="fa fa-weixin weixin"></i>
+                            </a>
+                           <a href="javascript:void(0)" class="share-circle">
+                                <i class="fa fa-weibo weibo"></i>
+                            </a>
+                            <a href="javascript:void(0)" class="share-circle">
+                                <i class="fa fa-picture-o picture"></i>
+                            </a>
+                            <a href="javascript:void(0)" class="share-circle more-share">
+                                更多分享
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--悬浮的操作框-->
+            <div class="side-bar"></div>
+            <!--对应的推荐阅读-->
+            <div class="note-bottom"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import myHeader from '../../components/myHeader'
+    export default {
+        data(){
+            return{
+                follow:{
+                   follow1:true,
+                   follow2:false
+                },
+                icon:{
+                   'fa-plus':true,
+                   'fa-check':false
+                },
+                active:{
+                    'like-group':true,
+                    activeClass:false
+                },
+                isColor:false
+            }
+        },
+        components:{
+            myHeader
+        },
+        methods:{
+            haveSeen:function () {
+                let word = this.$refs.see.innerHTML;
+                //这里预留关注的ajax操作
+                this.follow.follow1 = !this.follow.follow1;
+                this.follow.follow2 = !this.follow.follow2;
+                this.icon["fa-plus"] = !this.icon["fa-plus"];
+                this.icon["fa-close"] = !this.icon["fa-close"];
+                this.$refs.see.innerHTML = word == '关注' ? "已关注" : "关注"
+                this.$refs.see2.innerHTML = word == '关注' ? "已关注" : "关注"
+            },
+            mouseenter:function () {
+                let word = this.$refs.see.innerHTML
+                if (word == "已关注"){
+                    this.$refs.icon.className = "fa fa-close"
+                    this.$refs.see.innerHTML ="取消关注"
+                    this.$refs.icon2.className = "fa fa-close"
+                    this.$refs.see2.innerHTML ="取消关注"
+                }
+            },
+            mouseleave:function(){
+                let word = this.$refs.see.innerHTML
+                if (word == "取消关注"){
+                    this.$refs.icon.className = "fa fa-check"
+                    this.$refs.see.innerHTML ="已关注"
+                    this.$refs.icon2.className = "fa fa-check"
+                    this.$refs.see2.innerHTML ="已关注"
+                }
+            },
+            changeActive:function () {
+                let num = parseInt(this.$refs.spanNum.innerHTML);
+                console.log(num)
+                if(this.active["like-group"] ==true){
+                    this.active["like-group"] = !this.active["like-group"];
+                    this.active.activeClass = !this.active.activeClass;
+                    this.isColor = !this.isColor;
+                    this.$refs.spanNum.innerHTML = num + 1;
+                }
+                else{
+                    this.active["like-group"] = !this.active["like-group"];
+                    this.active.activeClass = !this.active.activeClass;
+                    this.isColor = !this.isColor;
+                    this.$refs.spanNum.innerHTML = num - 1;
+                }
+            }
+        }
+    }
+</script>
+
+<style></style>
